e6ac4ccfbb607a429b8311903ac67ea2ff9a0d31
Diablo <gangurderitesh375@gmail.com>
2026-08-26 17:02:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/analyze': {
-        target: 'http://localhost:8000',
+        target: 'http://aiplag-production.up.railway.app',
         changeOrigin: true,
       },
       '/subscribe': {
-        target: 'http://localhost:8000',
+        target: 'http://aiplag-production.up.railway.app',
         changeOrigin: true,
       },
     },
